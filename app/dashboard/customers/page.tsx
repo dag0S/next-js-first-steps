2fe47aeb56e08501fe53deecda137ee655e916f5
Gosudarev Danila <danidagosudarev@gmail.com>
2024-10-19 01:58:35 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return <p>Customer Page</p>;
+};
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return <p>Dashboard Page</p>;
+};
+
+export default Page;
